36669f74d9f2295ca47ffefe4154c93c1ced4876
hh <info@chiralsoftware.com>
2021-09-09 16:53:51 -0700

    fixed byte alignment in the Picture object, it now outputs. Plus added some hints about how to deal with byte alignment in structs.

--- LinkerWebp/src/main/java/chiralsoftware/linkerwebp/Picture.java
+++ LinkerWebp/src/main/java/chiralsoftware/linkerwebp/Picture.java
@@ -12,7 +12,24 @@ import static jdk.incubator.foreign.MemoryLayout.ofValueBits;
 import jdk.incubator.foreign.MemorySegment;
 
 /**
- * Represent the WebpPicture struct
+ * Represent the WebpPicture struct.
+ * <h1>Hints on debugging struct alignment</h1>
+ * Just copying the elements from the C struct will not produce correct alignment.
+ * First, check the structure size:
+ * <pre>    printf("size of Picture is: %lu\n", sizeof(struct WebPPicture));</pre>
+ * If:
+ * <pre>Picture.byteSize()</pre>
+ * does not equal that number, the GroupLayout is not right and it will crash.
+ * To figure this out, look at the size of PathElements using:
+ * <pre>final StringBuilder sb = new StringBuilder();
+        for(MemoryLayout m : Picture.memberLayouts()) {
+            sb.append(m.name().orElse("(none)")).append(", size: ").
+                    append(m.byteSize()).append(lineSeparator());
+        }</pre>
+        * And then compare this to the C struct by downloading and compiling libweb and then using:
+ * 
+ * <pre>pahole ./src/enc/.libs/libwebpencode_la-picture_enc.o<pre>
+ * This will quickly reveal where the problem is.
  */
 public final class Picture {
 
@@ -29,42 +46,64 @@ public final class Picture {
             // height
             ofValueBits(32, ByteOrder.nativeOrder()).withName("height"),
             // pointers to uint8_t (unsigned byte) luma / chroma planes
-            ofValueBits(8*8, ByteOrder.nativeOrder()).withName("y"),
-            ofValueBits(8*8, ByteOrder.nativeOrder()).withName("u"),
-            ofValueBits(8*8, ByteOrder.nativeOrder()).withName("v"),
+            ofValueBits(8*8, nativeOrder()).withName("y"),
+            ofValueBits(8*8, nativeOrder()).withName("u"),
+            ofValueBits(8*8, nativeOrder()).withName("v"),
             // luma/chroma strides.
             ofValueBits(32, ByteOrder.nativeOrder()).withName("y_stride"),
             ofValueBits(32, ByteOrder.nativeOrder()).withName("uv_stride"),
-            ofPaddingBits(3 * 32), // padding for later use
             // pointer to the alpha plane uint8_t 
             ofValueBits(8*8, ByteOrder.nativeOrder()).withName("a"),
             ofValueBits(32, ByteOrder.nativeOrder()).withName("a_stride"),
+            ofPaddingBits(2 * 32), // padding for later use
+            ofPaddingBits(4 * 8), // this showed up when using pahole
             // Alternate ARGB input, recommended for lossless compression.
             //
             // Used if use_argb = 1.
             // Pointer to argb (32 bit) plane, uint32_t* argb
             ofValueBits(8*8, ByteOrder.nativeOrder()).withName("argb"),
             // This is stride in pixels units, not bytes.
-            ofValueBits(32, ByteOrder.nativeOrder()).withName("argb_stride"),
+            ofValueBits(32, nativeOrder()).withName("argb_stride"),
+            ofPaddingBits(3 * 32), // padding for later use
+            
+            // OUTPUT
             // Byte-emission hook, to store compressed bytes as they are ready.
-            ofValueBits(8*8, ByteOrder.nativeOrder()).withName("writer"), // can be null
-            ofValueBits(8*8, ByteOrder.nativeOrder()).withName("custom_ptr"), // *void
-            ofValueBits(8*8, ByteOrder.nativeOrder()).withName("extra_info_type"), 
-            ofValueBits(8*8, ByteOrder.nativeOrder()).withName("extra_info"), // pointer to extra info 
+            ofValueBits(8*8, nativeOrder()).withName("writer"), // can be null
+            ofValueBits(8*8, nativeOrder()).withName("custom_ptr"), // *void
+            ofValueBits(32, nativeOrder()).withName("extra_info_type"), 
+            ofPaddingBits(4*8), // from pahole
+            ofValueBits(8*8,nativeOrder()).withName("extra_info"), // pointer to extra info 
             ofValueBits(8*8, nativeOrder()).withName("stats"), // WebPAuxStats* stats
             // Error code for the latest error encountered during encoding
             ofValueBits(32, ByteOrder.nativeOrder()).withName("error_code"),
+            ofPaddingBits(4*8), // from pahole
             ofValueBits(8*8, nativeOrder()).withName("progress_hook"), // WebPProgressHook
             ofValueBits(8*8, nativeOrder()).withName("user_data"), // void* user_data
             ofPaddingBits(32 * 3), // padding for later use
-            ofPaddingBits(32), // *pad4
-            ofPaddingBits(32), // *pad5
+            ofPaddingBits(4*8), // from pahole
+            ofPaddingBits(8*8), // *pad4
+            ofPaddingBits(8*8), // *pad5
             ofPaddingBits(32 * 8), // pad6
             // PRIVATE FIELDS
             ofValueBits(8*8, nativeOrder()).withName("memoyr_"), // row chunk of memory for yuv
             ofValueBits(8*8, nativeOrder()).withName("memory_argb_"), // and for argb
-            ofValueBits(32 * 2, nativeOrder()) // padding for later use
+            ofValueBits(8*8 * 2, nativeOrder()) // padding for later use
     ).withBitAlignment(64);
+    
+    static {
+//        LOG.info("The size of the picture memory arrangment is: " + Picture.byteSize() + ", which should be 256");
+//        if(Picture.byteSize() != 256)
+//            LOG.warning("OH NO! WRONG PICTURE SIZE!");
+//        LOG.info("The writer offset is: " + Picture.byteOffset(groupElement("writer")));
+//        LOG.info(" attributes: " + Picture.attributes().collect(joining(", ")));
+//        final StringBuilder sb = new StringBuilder();
+//        for(MemoryLayout m : Picture.memberLayouts()) {
+//            sb.append(m.name().orElse("(none)")).append(", size: ").
+//                    append(m.byteSize()).append(lineSeparator());
+//        }
+//        out.println("The layout: ");
+//        out.println(sb);
+    }
 
     public Picture(MemorySegment segment) {
         if(segment == null) throw new NullPointerException("segment can't be null");
--- LinkerWebp/testing.c
+++ LinkerWebp/testing.c
@@ -1,7 +1,11 @@
 // A file to test some things
+#include <stdio.h>
 #include <webp/encode.h>
 
 int main(int argc, char *argv[]) {
+
+    printf("Ok let's test\n");
+    printf("size of Picture is: %lu\n", sizeof(struct WebPPicture));
 
 }
 
